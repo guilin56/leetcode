0c28bc013520b4eb0366d874c4ba9a01c18545ca
Parallel universes <2320066162@qq.com>
2024-07-19 09:13:29 +0800

    0719

---
+++ leet3096.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class leet3096 {
+    public static void main(String[] args) {
+        int [] arr=new int[]{1,1};
+        int i = minimumLevels(arr);
+        System.out.println(i);
+    }
+
+    public static int minimumLevels(int[] possible) {
+        int count=0,count1=0;
+        for(int i=0;i<possible.length;i++){
+         count1+=(possible[i]>0?1:-1);
+        }
+        for(int i=0;i<possible.length-1;i++){
+            if (possible[i]>0){
+                count++;
+                count1--;
+            }else{
+                count--;
+                count1++;
+            }
+            if(count>count1){
+                return i+1;
+            }
+
+        }
+
+        return -1;
+    }
+
+}
