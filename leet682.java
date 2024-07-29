e3bea62c9804e722be252dbb7d65bafe7c95b0a5
Parallel universes <2320066162@qq.com>
2024-07-29 09:49:03 +0800

    0729

---
+++ leet682.java
@@ -0,0 +1,33 @@
+
+import java.util.Stack;
+
+//栈的应用，之前用的比较少
+public class leet682 {
+    public static void main(String[] args) {
+        String[] ops = {"5", "2", "C", "D", "+"};
+        String[] test = {"36", "28", "70", "65", "C", "+", "33", "-46", "84", "C"};
+        System.out.println(calPoints(test));
+    }
+
+    public static int calPoints(String[] operations) {
+        int result = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].equals("+")) {
+                Integer pop = stack.pop();
+                int last = stack.peek() + pop;
+                stack.push(pop);
+                stack.push(last);
+            } else if (operations[i].equals("D")) {
+                stack.push(stack.peek() * 2);
+            } else if (operations[i].equals("C")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.valueOf(operations[i]));
+            }
+        }
+        for (Integer i : stack)
+            result += i;
+        return result;
+    }
+}
